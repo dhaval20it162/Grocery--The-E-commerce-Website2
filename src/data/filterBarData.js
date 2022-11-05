f8ded5ec8bf60f430c9c0b5f6dc5d51ba706bdcb
dhaval20it162 <80237933+dhaval20it162@users.noreply.github.com>
2022-11-05 12:41:02 +0530

    1st commit
    1st commit

---
+++ src/data/filterBarData.js
@@ -0,0 +1,50 @@
+export const sortMenu = [
+    "Latest",
+    "Featured",
+    "Top Rated",
+    "Price(Lowest First)",
+    "Price(Highest First)"
+];
+
+export const filterMenu = [
+    {
+        id: 1,
+        title: "Brands",
+        menu: [
+            "AASHIRVAAD",
+            "TATA",
+            "PATANJALI"
+        ],
+    },
+    {
+        id: 2,
+        title: "Category",
+        menu: [
+            "RICE AND ATTA",
+            "OIL AND MASALA",
+            "DALS,PULSES AND GRAINS",
+            "BISCUITS"
+        ],
+    },
+    {
+        id: 3,
+        title: "Type",
+        menu: [
+            "1 KG",
+            "2 KG",
+            "5 KG",
+        ],
+    },
+    {
+        id: 4,
+        title: "Price",
+        menu: [
+            "500 & Below",
+            "501 - 1,000",
+            "1,001 - 1,500",
+            "1,501 - 3,000",
+            "3,001 - 5,000",
+            "5,001 & Above"
+        ],
+    },
+];
---
+++ src/data/reviewsData.js
@@ -0,0 +1,39 @@
+const reviewsData = [
+    {
+        id: 1,
+        name: "Atharva Kumar",
+        date: "4 Aug 2022",
+        review: "BEST TASTE, love it.",
+        rateCount: 5,
+    },
+    {
+        id: 2,
+        name: "Ritika Sen",
+        date: "15 July 2022",
+        review: "VERY GOOD PRODUCT",
+        rateCount: 5,
+    },
+    {
+        id: 3,
+        name: "Bhavesh Joshi",
+        date: "10 June 2022",
+        review: "Super amazing product !!!",
+        rateCount: 4,
+    },
+    {
+        id: 4,
+        name: "Anandi Gupta",
+        date: "6 May 2022",
+        review: "GET BUT ,YOU SHOULD BUY LOOSE",
+        rateCount: 4,
+    },
+    {
+        id: 5,
+        name: "Arif Khan",
+        date: "27 April 2022",
+        review: "Very good but still has flaws!",
+        rateCount: 3,
+    },
+]
+
+export default reviewsData;
